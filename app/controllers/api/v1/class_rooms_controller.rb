be9cf6437c97ec3c5b38c243f5534b797d1172b6
Vladimir Ostapenko <ostapenkov74@gmail.com>
2024-03-06 10:56:34 +0500

    fix params

--- app/controllers/api/v1/class_rooms_controller.rb
+++ app/controllers/api/v1/class_rooms_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ClassRoomsController < Api::BaseController
       def index
-        school ||= School.includes(:class_rooms).find(params[:class_room][:school_id])
+        school ||= School.includes(:class_rooms).find(params[:school_id])
         
         render_success school.class_rooms
       end
